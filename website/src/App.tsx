0874a25043c137fd3529d501e21bdec36e988875
Kristin Mickols <kmickols@kth.se>
2023-02-12 17:28:44 +0100

    cleanup needed, navbar, icon, schema

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,15 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="djubdot">
-          dJubileet, loading...
-        </p>
-        Scroll down to play!
+    <header className="App-header">
+    <div className="topnav">
+  <a className="active" href="#home">Home</a>
+  <a href="#about">About</a>
+</div> 
+<p className="header">dJubileet 2023</p>
+
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <iframe src="https://funhtml5games.com?embed=spaceinvaders" className="game" scrolling="no"></iframe>
+      <p className="center">
+        <table className="schema">
+          <tr> 
+            <th>Schema</th></tr>
+            <tr>29/4 Halvårsfesten</tr>
+            <tr>25/9 dJubileumsfirandet börjar</tr>
+            <tr> 26/9 TBA</tr>
+            <tr>30/9 Banquette</tr>
+            <tr>4/10 XXL pub</tr>
+            <tr>7/10 Slutfesten</tr>
+        </table>
+      </p>
     </div>
+
   );
 }
 
