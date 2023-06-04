0e6eb4f6f2e1280a2b62992d4333bd768128ad6f
Max Wippich <wippich@kth.se>
2023-06-04 22:29:00 +0200

    fixed text for HF, added graying out of events that have already happened

--- website/src/components/EventEntry.tsx
+++ website/src/components/EventEntry.tsx
@@ -1,12 +1,18 @@
 import React, {useState} from "react";
 
-function EventEntry({date, title, timeAndPlace = "", fbLink = "", ticketLink = "", interestLink = "", info = "", ticketUnreleased = false, ticketSoldOut = false, noTicket = false} :
-                        {date : string, title : string, timeAndPlace? : string, fbLink? : string, ticketLink? : string, interestLink? : string, info? : string, ticketUnreleased? : boolean, ticketSoldOut? : boolean, noTicket? : boolean} ) {
+function EventEntry({date, title, timeAndPlace = "", fbLink = "", ticketLink = "", interestLink = "", info = "", ticketUnreleased = false, ticketSoldOut = false, noTicket = false, dateTime} :
+                        {date : string, title : string, timeAndPlace? : string, fbLink? : string, ticketLink? : string, interestLink? : string, info? : string,
+                            ticketUnreleased? : boolean, ticketSoldOut? : boolean, noTicket? : boolean, dateTime : Date} ) {
 
     const [expanded, setExpanded] = useState(false);
 
+    const today = new Date();
+    let hasHappened : boolean = !(dateTime.getFullYear() > today.getFullYear() ||
+        (dateTime.getMonth() > today.getMonth() && dateTime.getFullYear() == today.getFullYear()) ||
+        (dateTime.getDay() > today.getDay() && dateTime.getMonth() == today.getMonth() && dateTime.getFullYear() == today.getFullYear()));
+
     return (
-        <div className={"event-row"} onClick={() => setExpanded(!expanded)}>
+        <div className={hasHappened?"disabled-event-row":"event-row"}  onClick={() => setExpanded(!expanded)}>
             <td className={"event-date"}>
                 <p>{date}</p>
                 <p className={"event-date-sub"} hidden={timeAndPlace === ""}>{timeAndPlace}</p>
@@ -16,17 +22,17 @@ function EventEntry({date, title, timeAndPlace = "", fbLink = "", ticketLink = "
                     {title}
                 </p>
                 <div>
-                    {!(ticketLink === "" || ticketUnreleased)?<a className={"App-schedule-link"} href={ticketLink} target="_blank" onClick={(e) => e.stopPropagation()}>
+                    {!(ticketLink === "" || ticketUnreleased || hasHappened)?<a className={"App-schedule-link"} href={ticketLink} target="_blank" onClick={(e) => e.stopPropagation()}>
                         {ticketSoldOut?"Biljetter (Slutsålt)":"Biljetter"}
                     </a>:null}
-                    {interestLink !== ""?<a className={"App-schedule-link"} href={interestLink} target="_blank" onClick={(e) => e.stopPropagation()}>
+                    {interestLink !== "" && !hasHappened?<a className={"App-schedule-link"} href={interestLink} target="_blank" onClick={(e) => e.stopPropagation()}>
                         Reservlista
                     </a>:null}
-                    {ticketUnreleased?<a className={"disabled-link"} >
+                    {ticketUnreleased && !hasHappened?<a className={"disabled-link"} >
                         Biljetter (Kommer)
                     </a>:null}
                     {noTicket?(<span>Ingen biljett behövs!</span>):null}
-                    {fbLink !== ""?<a className={"App-schedule-link"} href={fbLink} target="_blank" onClick={(e) => e.stopPropagation()} hidden={fbLink === ""}>
+                    {fbLink !== ""?<a className={hasHappened?"disabled-App-schedule-link":"App-schedule-link"} href={fbLink} target="_blank" onClick={(e) => e.stopPropagation()} hidden={fbLink === ""}>
                         Facebook
                     </a>:null}
                 </div>
