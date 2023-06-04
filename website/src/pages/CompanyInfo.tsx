918b4796d347b0cba6a44cf2812c673f75b6986e
Max Wippich <wippich@kth.se>
2023-06-04 23:16:48 +0200

    spelling

--- website/src/pages/CompanyInfo.tsx
+++ website/src/pages/CompanyInfo.tsx
@@ -4,7 +4,7 @@ function CompanyInfo() {
     return (
         <div className={"App-about"}>
             <h1 className={"title"}>För Företag</h1>
-            <p> Är ert företag intresserat av att sponsra datasektionens största event? Kika på vår produktkatalog som ni hittar på länken nedan och maila oss vid intresse eller frågor!</p>
+            <p> Är ert företag intresserat av att sponsra Datasektionens största event? Kika på vår produktkatalog som ni hittar på länken nedan och maila oss vid intresse eller frågor!</p>
             <a className={"download-link"} href={"https://drive.google.com/uc?export=download&id=1yBpJpzCYg75xA8GNvOkUghwJJeGZKuQg"}>Produktkatalog</a>
             <p> Har ni något annat förslag på samarbete? Skriv ett mail till oss så diskuterar vi det gärna!</p>
             <a className={"download-link"} href={"mailto: sales@djubileet.se"}>sales@djubileet.se</a>
