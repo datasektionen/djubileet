9b1d476b7be1af1da3db898f3659daed7b40451a
Max Wippich <wippich@kth.se>
2023-07-23 20:02:02 +0200

    changed catalogue to reception-safe version

--- website/src/pages/CompanyInfo.tsx
+++ website/src/pages/CompanyInfo.tsx
@@ -5,7 +5,7 @@ function CompanyInfo() {
         <div className={"App-about"}>
             <h1 className={"title"}>För Företag</h1>
             <p> Är ert företag intresserat av att sponsra Datasektionens största event? Kika på vår produktkatalog som ni hittar på länken nedan och maila oss vid intresse eller frågor!</p>
-            <a className={"download-link"} href={"https://drive.google.com/uc?export=download&id=1yBpJpzCYg75xA8GNvOkUghwJJeGZKuQg"}>Produktkatalog</a>
+            <a className={"download-link"} href={"https://drive.google.com/u/0/uc?id=1uebbQCHH3YlEBNa8uC9BocTjPcvjloZI&export=download"}>Produktkatalog</a>
             <p> Har ni något annat förslag på samarbete? Skriv ett mail till oss så diskuterar vi det gärna!</p>
             <a className={"download-link"} href={"mailto: sales@djubileet.se"}>sales@djubileet.se</a>
             <p></p>
