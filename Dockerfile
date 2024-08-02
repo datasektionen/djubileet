24b57714b09fdc3dbaf6a60f966bf79fc84162c5
Mathias Magnusson <mathias@magnusson.space>
2024-08-02 20:06:15 +0200

    🧹
    Blev trött på att en statisk sida ska ta 500MiB att köra FÖR ATT DEN KÖR
    CREATE REACT APPS DEV SERVER I PROD och tog bort lite mer skit när jag
    ändå var inne och härjade. Vad annars ska man göra en fredag kväll?

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
-FROM node:19-alpine
-USER node
-RUN mkdir -p /home/node/app
-WORKDIR /home/node/app
-COPY --chown=node website .
-RUN yarn install
-#RUN yarn build
+FROM node:20-alpine3.20 AS build
+WORKDIR /app
+
+COPY website/package.json website/package-lock.json ./
+RUN npm ci
+
+COPY website .
+RUN npm run build
+
+FROM busybox:1-musl
+WORKDIR /app
+
+RUN echo "E404:/app/index.html" > /httpd.conf
+COPY --from=build /app/build /app
+
 EXPOSE 3000
-CMD ["yarn", "start"]
+CMD ["httpd", "-fp3000", "-c/httpd.conf"]
--- website/src/pages/BigCountdown.tsx
+++ website/src/pages/BigCountdown.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import animatedLogo from '../img/AnimeradLogga.gif';
-import stars from '../img/stars30.webp';
 import '../css/BigCoundown.css';
 import CountdownTimer from '../components/CountdownTimer';
 
 function BigCountdown() {
     const DATUM = new Date("2028-10-14T15:00:00Z");
 
-    return (
-        <div className="Wrapper" >
+    return <>
+        <video className="stars-bg" src={`${process.env.PUBLIC_URL}/stars30.webm`} autoPlay loop muted playsInline controls={false} />
+        <div className="Wrapper">
 
-            <img  src={animatedLogo} alt={"dJubileet Logo"} width={700} />
+            <img src={animatedLogo} alt={"dJubileet Logo"} width={700} />
 
             <div className={"LargeCountdown"}>
-                <CountdownTimer targetDate={DATUM}/>
+                <CountdownTimer targetDate={DATUM} />
             </div>
         </div>
-    );
+    </>;
 }
 
 export default BigCountdown;
